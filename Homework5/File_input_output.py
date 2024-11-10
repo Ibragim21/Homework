3f00982c563a5a15d608a34bcce8589a14445c82
ibragim21 <ibragimov.i2006@gmail.com>
2024-11-10 23:26:54 +0500

    fully completed h/w 4, file exception tasks finished

---
+++ Homework5/File_input_output.py
@@ -0,0 +1,35 @@
+# task1
+# with open('Homework5/test.txt', mode='r') as file:
+#     text=file.read()
+#     print(text)
+# task2
+x=int(input('enter a number: '))
+def read_lines():
+    with open('Homework5/test.txt', mode='r') as file:
+        line=file.readlines()
+        print(line[:x])
+read_lines()
+# task3
+
+# task4
+
+# task5
+
+# task6
+
+# task7
+
+# task8
+# task9
+# task10
+# task11
+# task12
+# task13
+# task14
+# task15
+# task16
+# task17
+# task18
+# task19
+# task20
+# task21
